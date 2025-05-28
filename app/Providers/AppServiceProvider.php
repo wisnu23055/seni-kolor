df44c2e6fba67795d5327019f6f86dd9a4718415
wisnu23055 <wisnu.23055@mhs.unesa.ac.id>
2025-05-28 22:43:12 +0700

    Set application timezone to Asia/Jakarta and configure Carbon locale to Indonesian

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Set default timezone untuk Carbon
+        Carbon::setLocale('id'); // Bahasa Indonesia
+        
+        // Set default timezone untuk semua instance Carbon
+        config(['app.timezone' => 'Asia/Jakarta']);
+        date_default_timezone_set('Asia/Jakarta');
     }
 }
